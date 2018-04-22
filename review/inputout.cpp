ed9bf0fbd58a9120fc3e0c4bea74b6de7565bc68
cjoen <cj00wen@gmail.com>
2018-04-22 13:32:51 -0700

    finished 9.1 hw

--- review/inputout.cpp
+++ review/inputout.cpp
@@ -2,10 +2,14 @@
 //assignment [NUMBER][WEEK]
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-
+  string yourName;
+  cout << "please enter your name:";
+  cin >> yourName;
+  cout << "Hello " << yourName << endl;
 
   return 0;
 }
--- wk2/hw9_1.cpp
+++ wk2/hw9_1.cpp
@@ -1,12 +1,45 @@
-//Christopher Owen Team H
-//assignment [NUMBER][WEEK]
+// This program demonstrates the use of pointer variables
+// It finds the area of a rectangle given length and width
+// It prints the length and width in ascending order
+
+// Christopher Owen
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int main() {
+int main()
+{
 
+	int length;	// holds length
+	int width;	// holds width
+	int area;	// holds area
 
-  return 0;
+	int *lengthPtr = nullptr;	// int pointer which will be set to point to length
+	int *widthPtr = nullptr;	// int pointer which will be set to point to width
+
+	cout << "Please input the length of the rectangle" << endl;
+	cin >> length;
+
+	cout << "Please input the width of the rectangle" << endl;
+	cin >> width;
+
+	lengthPtr = &length;
+  widthPtr = &width;
+
+	// Fill in code to make widthPtr point to width (hold its address)
+
+	area =	*lengthPtr * *widthPtr;
+
+	cout << "The area is " << area << endl;
+
+	if (*lengthPtr > *widthPtr){
+		cout << "The length is greater than the width" << endl;
+  }
+	else if (*lengthPtr < *widthPtr) {
+		cout << "The width is greater than the length" << endl;
+  }
+	else {
+		cout << "The width and length are the same" << endl;
+  }
+	return 0;
 }
