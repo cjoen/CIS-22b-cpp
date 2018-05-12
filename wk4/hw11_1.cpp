d9365975c583eff5e90c500c16be234c2c2d3cfc
cjoen <cj00wen@gmail.com>
2018-05-12 14:29:27 -0700

    wk 4 finished up and wk 5 initiated

--- wk4/hw11_1.cpp
+++ wk4/hw11_1.cpp
@@ -1,10 +1,69 @@
 //Christopher Owen Team H
-//assignment [NUMBER][WEEK]
+//assignment 11.1 Week 4
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+
 using namespace std;
 
+struct MovieData {
+    string title;
+    string director;
+    int year;
+    int runTime; //in minutes
+    double prodCosts;
+    double yearRev;
+};
+
+void fillStruct (MovieData *m){
+  cout << "Please enter the data of your favourit movie below " << endl;
+  cout << "Please enter the movie Title: ";
+  cin >> m->title;
+  cout << "Please enter the movie director: ";
+  cin >> m->director;
+  cout << "Please enter the movie year: ";
+  cin >> m->year;
+  cout << "Please enter the movie runtime (in minutes): ";
+  cin >> m->runTime;
+  cout << "Please enter the movie production cost: ";
+  cin >> m->prodCosts;
+  cout << "Please enter the First year revenue: ";
+  cin >> m->yearRev;
+
+}
+
+void printStruct(MovieData *m){
+  //do you just print each .value of each element in struct
+  cout << "Title   : " << m->title << endl;
+  cout << "Director   : " << m->director << endl;
+  cout << "Released   : " << m->year << endl;
+  cout << "Runtime  : " << m->runTime << " minutes" << endl;
+  cout << "Production cost : $" << m->prodCosts << ".00" << endl;
+  cout << "First Year Revenue : $" << m->yearRev << ".00" << endl;
+
+  double profits = m->yearRev - m->prodCosts;
+  if (profits > 0){
+    cout << "The first year profit : $" << profits << ".00" << endl << endl;
+  } else {
+    cout << "The first year loss : $" << profits << ".00" << endl << endl;
+  }
+
+}
+
 int main() {
+
+// set structure data fields
+    MovieData movie1, movie2;
+
+// get movie data using a function.
+  fillStruct(&movie1);
+  fillStruct(&movie2);
+
+  //print movie data using print function
+  printStruct(&movie1);
+  printStruct(&movie2);
+
 
 
   return 0;
--- wk4/hw11_2.cpp
+++ wk4/hw11_2.cpp
@@ -1,11 +1,46 @@
 //Christopher Owen Team H
-//assignment [NUMBER][WEEK]
+//assignment 11.2 week 4
 
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
-int main() {
+struct  player {
+  string name;
+  int number;
+  int score;
+};
 
+//request data for each player
+void getData (player *p) {
+  cout << "Please enter a name for the player: ";
+  cin >> p->name;
+  cout << "Please enter a player number: ";
+  cin >> p->number;
+  cout << "Please enter the players score: ";
+  cin >> p->score;
+}
+
+int main() {
+  //define main variables
+  int SIZE = 12;
+  player team[SIZE];
+  player winner;
+  int scoreSum = 0;
+
+  //loop through player array getting and processing data
+  for (int i = 0; i < SIZE; i++) {
+    getData(&team[i]);
+    scoreSum += team[i].score;
+    if (team[i].score > winner.score) {
+      winner = team[i];
+    }
+  }
+
+  //outputting processed data.
+  cout << "Sum of scores: " << scoreSum << endl;
+  cout << "The winner is: " << winner.name << " player number: " << winner.number << endl;
 
   return 0;
 }
